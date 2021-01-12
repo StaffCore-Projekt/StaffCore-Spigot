9acfb84714882210e6194397b236a62e191b93d7
ViaEnder <63646453+ViaEnder@users.noreply.github.com>
2021-01-12 18:52:52 +0100

    Fix GlobalMute

--- src/de/lacodev/rsystem/commands/CMD_MuteGlobal.java
+++ src/de/lacodev/rsystem/commands/CMD_MuteGlobal.java
@@ -14,28 +14,31 @@ public class CMD_MuteGlobal implements CommandExecutor {
         if (sender instanceof Player){
             Player p = (Player) sender;
             if( p.hasPermission(Main.getPermissionNotice("Permissions.Everything")) || p.hasPermission(Main.getPermissionNotice("Permissions.Mute.Global.Change")) ) {
-                if (args[0].equalsIgnoreCase("on")){
-                    if (BanManager.isGMute()){
-                        p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Already-Disabled"));
-                    }else{
-                        BanManager.setGlobalMute(true);
-                        p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Disable"));
-                        Bukkit.broadcastMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Start-Mute").replace("%player%", p.getName()));
-
-                    }
-                }else if (args[0].equalsIgnoreCase("off")){
-                    if (BanManager.isGMute()){
-                        BanManager.setGlobalMute(false);
-                        p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Enable"));
-                        Bukkit.broadcastMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Stop-Mute").replace("%player%", p.getName()));
-                    }else{
-                        p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Already-Enabled"));
-                    }
-                }else if (args[0].equalsIgnoreCase("info")) {
-                    if (BanManager.isGMute()){
-                        p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.IsEnabled"));
-                    }else{
-                        p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.IsDisabled"));
+                if (args.length == 1) {
+                    if (args[0].equalsIgnoreCase("on")) {
+                        if (BanManager.isGMute()) {
+                            p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Already-Disabled"));
+                        } else {
+                            BanManager.setGlobalMute(true);
+                            p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Disable"));
+                            Bukkit.broadcastMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Start-Mute").replace("%player%", p.getName()));
+                        }
+                    } else if (args[0].equalsIgnoreCase("off")) {
+                        if (BanManager.isGMute()) {
+                            BanManager.setGlobalMute(false);
+                            p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Enable"));
+                            Bukkit.broadcastMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Stop-Mute").replace("%player%", p.getName()));
+                        } else {
+                            p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Already-Enabled"));
+                        }
+                    } else if (args[0].equalsIgnoreCase("info")) {
+                        if (BanManager.isGMute()) {
+                            p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.IsEnabled"));
+                        } else {
+                            p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.IsDisabled"));
+                        }
+                    } else {
+                        p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Usage"));
                     }
                 }else{
                     p.sendMessage(Main.getPrefix() + Main.getMSG("Messages.Global-Mute-System.Usage"));
